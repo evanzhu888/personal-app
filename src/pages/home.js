993b5495a654c0235ac1eb2d89021b601d1c66e2
Evan Zhu <evanzhu888@gmail.com>
2023-11-23 01:43:51 -0500

    fix typo

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,7 @@ export function Home() {
             <div className="text">
                 <h1>Hello. I am Evan Zhu</h1>
                 <p> I am a computer science student at the <strong>Georgia Institute of Technology</strong> with concentrations in <strong>Intelligence</strong> and <strong>Information Internetworks</strong>. I am passionate about learning more and enhancing my coding skills, AI/ML, and full-stack development.</p>
-                <p> Outside of computer science, I enjoy in cooking/trying new foods, working out and staying active, and watching comedies.  Feel free to reach out to me ezhu46@gatech.edu!</p>
+                <p> Outside of computer science, I enjoy cooking/trying new foods, working out/staying active, and watching comedies.  Feel free to reach out to me ezhu46@gatech.edu!</p>
             </div>
             <img src={pic} className="pic" alt="profile" />
         </div>
