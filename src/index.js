cf13b502061b3bbff0d02863f8bb9fbf0713fd98
Evan Zhu <evanzhu888@gmail.com>
2023-11-23 01:17:15 -0500

    added title

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.title = "Evan's Personal Site"
 root.render(
   <React.StrictMode>
     <App />
